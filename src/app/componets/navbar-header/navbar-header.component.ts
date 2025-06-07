0f665e3111a5764c2e1eaf30143d39a9468799a5
JP-019 <juliocgp30@gmail.com>
2025-06-07 11:29:09 -0600

    Linkeo-Bostrap-5.3

---
+++ src/app/componets/navbar-header/navbar-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-header',
+  imports: [],
+  templateUrl: './navbar-header.component.html',
+  styleUrl: './navbar-header.component.css'
+})
+export class NavbarHeaderComponent {
+
+}
---
+++ src/app/pages/Enlaces-secundarios/seguimiento-de-pedido/seguimiento-de-pedido.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seguimiento-de-pedido',
+  imports: [],
+  templateUrl: './seguimiento-de-pedido.component.html',
+  styleUrl: './seguimiento-de-pedido.component.css'
+})
+export class SeguimientoDePedidoComponent {
+
+}
