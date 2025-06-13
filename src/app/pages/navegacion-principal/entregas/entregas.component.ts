f26fffbb1ffe1b399d3cbc8afd597d5932aae89e
JP-019 <juliocgp30@gmail.com>
2025-06-12 20:50:16 -0600

    cambios en carosuel-home

---
+++ src/app/pages/navegacion-principal/entregas/entregas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entregas',
+  imports: [],
+  templateUrl: './entregas.component.html',
+  styleUrl: './entregas.component.css'
+})
+export class EntregasComponent {
+
+}
