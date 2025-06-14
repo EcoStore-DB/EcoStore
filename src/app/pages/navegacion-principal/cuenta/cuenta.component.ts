f057c9885f996d27db2786a8e1247fdd615b66d3
JP-019 <juliocgp30@gmail.com>
2025-06-13 20:00:17 -0600

    v.04

--- src/app/pages/navegacion-principal/cuenta/cuenta.component.ts
+++ src/app/pages/navegacion-principal/cuenta/cuenta.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // ¡Importante para ngModel!
 
 @Component({
   selector: 'app-cuenta',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cuenta.component.html',
   styleUrls: ['./cuenta.component.css']
 })
 export class CuentaComponent {
   activeTab: string = 'config';
+
+  productos: { nombre: string; precio: number }[] = [];
+
+  nuevoProducto = {
+    nombre: '',
+    precio: null
+  };
+
+  agregarProducto() {
+    
+  }
+
+  actualizarProducto(index: number) {
+    alert('Producto actualizado.');
+  }
+
+  eliminarProducto(index: number) {
+    this.productos.splice(index, 1);
+  }
 }
